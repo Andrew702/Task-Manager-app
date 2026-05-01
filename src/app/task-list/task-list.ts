1d29f06d0545c205e7b1db93e84cd4396cd133eb
Andrew Hany Sobhy <andrewhfarid@gmail.com>
2026-05-01 13:40:33 +0300

    fixed major bug in updating data

--- src/app/task-list/task-list.ts
+++ src/app/task-list/task-list.ts
@@ -12,6 +12,7 @@ import { Task } from '../app';
 import { AllTasks } from '../all-tasks/all-tasks';
 import { DoneTasks } from '../done-tasks/done-tasks';
 import { PendingTasks } from '../pending-tasks/pending-tasks';
+import { NotFoundError } from 'rxjs';
 @Component({
   selector: 'app-task-list',
   imports: [AllTasks, DoneTasks, PendingTasks],
@@ -28,17 +29,15 @@ export class TaskList implements OnChanges {
   TempTaskStorage!: Task;
 
   ngOnChanges(changes: SimpleChanges): void {
-    const RecievedTaskChange = changes['RecievedTask'];
-    console.log(RecievedTaskChange);
-    if (RecievedTaskChange.firstChange == false)
-      if (
-        RecievedTaskChange.currentValue != RecievedTaskChange.previousValue &&
-        this.EditMode == false
-      )
+    const TaskChanges = changes['RecievedTask'];
+    console.log(TaskChanges);
+    if (TaskChanges.firstChange == false)
+      if (TaskChanges.currentValue != TaskChanges.previousValue && this.EditMode == false)
         this.TaskArr.push({ ...this.RecievedTask });
   }
 
-  //when i send an update request to edit data
+  //made only for binding purpose
+  // when i send an update request to edit data
   OnRecievedTaskChange() {
     this.RecievedTaskChange.emit({ ...this.RecievedTask } as Task);
   }
@@ -52,16 +51,27 @@ export class TaskList implements OnChanges {
     CurrTask!.taskdone = !CurrTask?.taskdone;
   }
 
+  //updates Happen here
   StartUpdate(TasktoUpdate: Task) {
     this.EditMode = true;
-    this.TempTaskStorage = this.RecievedTask;
-    this.RecievedTask = TasktoUpdate;
-    this.OnRecievedTaskChange();
+    this.TempTaskStorage = { ...this.RecievedTask };
+    this.RecievedTask = { ...TasktoUpdate };
+    this.OnRecievedTaskChange(); //emit the current task
   }
 
   FinishUpdate() {
-    this.EditMode = false;
+    if (this.EditMode === false) return;
+    console.log('this.FinishUpdate');
+
+    //find current edited task using ID
     let UpdatedTask = this.TaskArr.find((Task) => Task.taskID === this.RecievedTask.taskID);
-    UpdatedTask = { ...this.RecievedTask };
+    if (UpdatedTask != undefined) {
+      console.log('found', UpdatedTask);
+      Object.assign(UpdatedTask, { ...this.RecievedTask });
+    }
+    // UpdatedTask = { ...this.RecievedTask };  //old invalid way of updating, errors because it reassigns reference to updated task
+    console.log(this.TaskArr);
+    Object.assign(this.RecievedTask, { ...this.TempTaskStorage }); //REstore old data //also uded object assign for same error
+    this.EditMode = false;
   }
 }
